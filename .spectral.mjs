a9a0b934a17c87c97e8863660fdcee4e7249c321
jsolares@tecniseguros.com <jsolares@tecniseguros.com>
2023-07-17 11:49:19 -0600

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMDc5Mw/spectral.js?branch_slug=master&token=52d240e6-0197-4cf8-a58b-1cb2c86bfffd";
+export default { extends: ruleset };
